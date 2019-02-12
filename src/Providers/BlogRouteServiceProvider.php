5b66bcc0194d15e8992cc511a98d1881fd466850
Cristian Benescu <cristian.benescu@plentydevelopment.com>
2019-02-12 11:52:30 +0200

    Keep both /article/ route and the one with the "article" removed until Terra changes

--- src/Providers/BlogRouteServiceProvider.php
+++ src/Providers/BlogRouteServiceProvider.php
@@ -26,6 +26,8 @@ class BlogRouteServiceProvider extends RouteServiceProvider
     {
         $router->get('blog/search/{searchString}', 'Blog\Controllers\BlogController@searchArticles');
         $router->get('blog/tag/{tagId}/{tagName?}', 'Blog\Controllers\BlogController@listArticlesByTag')->where('tagId', '\d+');
+        // TODO Remove the /article/ route when changes are made to the terra preview button
+        $router->get('blog/article/{urlName}', 'Blog\Controllers\BlogController@showArticle');
         $router->get('blog/{urlName}', 'Blog\Controllers\BlogController@showArticle');
 
         $apiRouter->version(['v1'], ['namespace' => 'Blog\Controllers'], function ($apiRouter)
