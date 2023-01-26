19a33914ac4fdcbbee6ad24cc0f6b5bd7022cc2f
Steve Tentrop <steve.tentrop@plentymarkets.com>
2023-01-26 10:56:47 +0100

    ADD: mocked data

--- src/Extensions/BlogSitemapPattern.php
+++ src/Extensions/BlogSitemapPattern.php
@@ -15,6 +15,21 @@ class BlogSitemapPattern
         /** @var PluginSeoSitemapService $seoSitemapService */
         $seoSitemapService = pluginApp(PluginSeoSitemapService::class);
 
-        $seoSitemapService->setBlogContent(['test' => "bla"]);
+        $seoSitemapService->setBlogContent([
+                [
+                    'publish_date' => '2023-01-25',
+                    'url' => 'https://kn71h80kvr3b.c14-01.plentymarkets.com/blog/blog-news/mein-erster-beitrag/',
+                    'title' => 'Mein erster Beitrag',
+                    'lang' => 'de',
+                    'keywords' => 'News,Rabatt'
+                ],
+                [
+                    'publish_date' => '2023-01-26',
+                    'url' => 'https://kn71h80kvr3b.c14-01.plentymarkets.com/blog/blog-news/mein-erster-beitrag/',
+                    'title' => 'Mein zweiter Beitrag',
+                    'lang' => 'de',
+                    'keywords' => 'News'
+                ]
+            ]);
     }
 }
