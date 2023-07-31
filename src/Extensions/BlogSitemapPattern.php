156a72c39fd83e50aa28286f6afe4e1a65640cfe
Razvan Bodnariu <razvan.bodnariu@plentymarkets.com>
2023-07-31 12:15:09 +0300

    reduced post data array

--- src/Extensions/BlogSitemapPattern.php
+++ src/Extensions/BlogSitemapPattern.php
@@ -6,19 +6,32 @@ use Plenty\Modules\Plugin\Events\LoadSitemapPattern;
 use Plenty\Modules\Plugin\Services\PluginSeoSitemapService;
 use Blog\AssistantServices\AssistantsService;
 
+
 class BlogSitemapPattern
 {
     /**
      * @param LoadSitemapPattern $sitemapPattern
      */
-    public function handle(LoadSitemapPattern $sitemapPattern)
+    public function handle(LoadSitemapPattern $sitemapPattern, BlogService $service)
     {
         /** @var PluginSeoSitemapService $seoSitemapService */
         $seoSitemapService = pluginApp(PluginSeoSitemapService::class);
 
         $assistantsService = pluginApp(AssistantsService::class);
         $dynamoPosts = $assistantsService->getDynamoDbPosts();
+        $result = [];
+        foreach($dynamoPosts as $post) {
+            $url = $service->buildFullPostUrl($post);
 
-        $seoSitemapService->setBlogContent($dynamoPosts);
+            $result[] = [
+                'publish_date' => date('Y-m-d', $post['data']['post']['publishedAt']),
+                'url' => $url,
+                'title' => $post['data']['post']['title'],
+                'lang' => $post['data']['post']['lang'],
+                'keywords' => $post['data']['data']['keywords'],
+            ];
+        }
+
+        $seoSitemapService->setBlogContent($result);
     }
 }
