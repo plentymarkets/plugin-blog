c7afb17d06de472c89ea1120c4895f82689f195c
plentydev <iulian.vladu@plentymarkets.com>
2023-08-01 11:52:27 +0300

    -conditions for published and active posts, added domain url

--- src/Extensions/BlogSitemapPattern.php
+++ src/Extensions/BlogSitemapPattern.php
@@ -6,6 +6,7 @@ use Blog\Services\BlogService;
 use Plenty\Modules\Plugin\Events\LoadSitemapPattern;
 use Plenty\Modules\Plugin\Services\PluginSeoSitemapService;
 use Blog\AssistantServices\AssistantsService;
+use Plenty\Modules\Webshop\Contracts\WebstoreConfigurationRepositoryContract;
 
 
 class BlogSitemapPattern
@@ -21,20 +22,27 @@ class BlogSitemapPattern
         /** @var PluginSeoSitemapService $seoSitemapService */
         $seoSitemapService = pluginApp(PluginSeoSitemapService::class);
 
+        /** @var WebstoreConfigurationRepositoryContract $webstoreConfigurationRepository */
+        $webstoreConfigurationRepository = pluginApp(WebstoreConfigurationRepositoryContract::class);
+        $domain = $webstoreConfigurationRepository->getWebstoreConfiguration()->domainSsl;
+
         /** @var AssistantsService $assistantsService */
         $assistantsService = pluginApp(AssistantsService::class);
         $dynamoPosts = $assistantsService->getDynamoDbPosts();
         $result = [];
-//        foreach($dynamoPosts as $post) {
-//            $url = $blogService->buildFullPostUrl($post);
-//            $result[] = [
-//                'publish_date' => date('Y-m-d', strtotime($post['data']['post']['publishedAt'])),
-//                'url' => $url['postUrl'],
-//                'title' => $post['data']['post']['title'],
-//                'lang' => $post['data']['post']['lang'],
-//                'keywords' => $post['data']['metaData']['keywords'],
-//            ];
-//        }
-        $seoSitemapService->setBlogContent($dynamoPosts);
+        foreach($dynamoPosts as $post) {
+            if ($post['data']['post']['active'] && $post['data']['post']['publishedAtHour']) {
+                $url = $blogService->buildFullPostUrl($post);
+                $result[] = [
+                    'publish_date' => date('Y-m-d', strtotime($post['data']['post']['publishedAt'])),
+                    'url' => $domain . $url['postUrl'],
+                    'title' => $post['data']['post']['title'],
+                    'lang' => $post['data']['post']['lang'],
+                    'keywords' => $post['data']['metaData']['keywords'],
+                ];
+            }
+
+        }
+        $seoSitemapService->setBlogContent($result);
     }
 }
